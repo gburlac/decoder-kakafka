9cbfe3c26083f0d7f65aa8efee9df48064900b9d
Gheorghe Burlac <56862163+gburlac@users.noreply.github.com>
2026-03-02 16:50:49 +0200

    Refactor main method variable names for clarity

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,34 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
-    public static void main(String[] args) {
-        String hexInput;
-        if (args.length > 0) {
-            hexInput = String.join(" ", args);
+    public static void main(String[] var0) {
+        String var1;
+        if (var0.length > 0) {
+            var1 = String.join(" ", var0);
         } else {
-            hexInput = "08 03 12 14 6d 61 72 6b 65 74 42 72 69 65 66 49 64 41 42 43 31 31 32 38 18 b1 a8 03";
+            var1 = "08 03 12 14 6d 61 72 6b 65 74 42 72 69 65 66 49 64 41 42 43 31 31 32 38 18 b1 a8 03";
         }
 
-        byte[] message = ProtoDecoder.parseHexString(hexInput);
+        byte[] var2 = ProtoDecoder.parseHexString(var1);
+        Map var3 = ProtoDecoder.decodeMessage(var2);
+        TreeMap var4 = new TreeMap(var3);
+        System.out.println("Decode byte: {");
+        int var5 = 0;
+        int var6 = var4.size();
 
-        Map<Integer, Object> decoded = ProtoDecoder.decodeMessage(message);
-        System.out.println("Decode byte: " + decoded);
+        for(Map.Entry var8 : var4.entrySet()) {
+            String var9 = String.valueOf(var8.getValue()).replace("\\", "\\\\").replace("\"", "\\\"");
+            String var10 = var5 < var6 - 1 ? "," : "";
+            System.out.println("  \"" + String.valueOf(var8.getKey()) + "\": \"" + var9 + "\"" + var10);
+            ++var5;
+        }
+
+        System.out.println("}");
     }
 }
